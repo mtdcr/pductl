048d631b42667b877eb4681bf756524d899716c4
Andreas Oberritter <obi@saftware.de>
2018-06-28 21:52:03 +0200

    setup.py: switch to setuptools

--- setup.py
+++ setup.py
@@ -1,16 +1,36 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+        long_description = fh.read()
 
 setup(
   name='atenpdu',
-  version='0.1',
+  version='0.1.1',
   author='Andreas Oberritter',
   author_email='obi@saftware.de',
   url='https://github.com/mtdcr/pductl',
   description='Interface for ATEN-PE PDUs',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
   download_url='https://github.com/mtdcr/pductl',
   license='MIT',
   py_modules=['atenpdu'],
   scripts=['pductl'],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Intended Audience :: System Administrators',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3 :: Only',
+    'Topic :: Home Automation',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Topic :: System :: Systems Administration',
+    'Topic :: Utilities',
+  ],
+  install_requires=[
+    'pysnmp>=4.3.2',
+  ],
 )
