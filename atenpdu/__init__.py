e2d1de4ed1acbd7a61bd818861a6a33d4d46773a
Andreas Oberritter <obi@saftware.de>
2025-10-04 16:16:01 +0200

    Fix MIB loading
    This is pretty ugly, but there you are. Pysnmp creates multiple
    MIB builders. Adjust the search path of the one used by our code
    paths.

--- atenpdu/__init__.py
+++ atenpdu/__init__.py
@@ -38,6 +38,7 @@ from pysnmp.hlapi.asyncio import (
     usmAesCfb128Protocol,
     usmHMACMD5AuthProtocol,
 )
+from pysnmp.hlapi.varbinds import MibViewControllerManager
 from pysnmp.hlapi.v3arch.asyncio.cmdgen import getCmd, setCmd
 from pysnmp.smi import builder
 
@@ -72,7 +73,10 @@ class AtenPE(object):
         else:
             self._auth_data = CommunityData(community)
         self._snmp_args: t.List[t.Any] = []
-        self._snmp_engine.get_mib_builder().addMibSources(builder.DirMibSource(Path(__file__).parent / "mibs"))
+
+        mibs_dir = Path(__file__).parent / "mibs"
+        mvc = MibViewControllerManager.get_mib_view_controller(self._snmp_engine.cache)
+        mvc.mibBuilder.add_mib_sources(builder.DirMibSource(mibs_dir))
 
     def close(self) -> None:
         self._snmp_engine.transportDispatcher.closeDispatcher()
